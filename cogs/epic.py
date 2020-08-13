7a830fc9f19d4c62ec95bdb68ea5c9be6c00cb9b
Boa <rubberboa01@gmail.com>
2020-08-13 02:04:31 -0700

    e
    basic formatting
    an embed_color variable will come in handy when wanting to change all embed colors when you have a large amount of commands with embeds

--- cogs/epic.py
+++ cogs/epic.py
@@ -2,22 +2,21 @@ import discord
 from discord.ext import commands
 import datetime
 
+embed_color = discord.Color(0x2F3136)
 
 class Epic(commands.Cog):
 
     def __init__(self, client):
-
         self.client = client
 
     # basic ping command cause why not lmao
     @commands.command()
-    @commands.cooldown(1,10, commands.BucketType.user)
+    @commands.cooldown(1, 10, commands.BucketType.user)
     async def ping(self, ctx):
 
-        embed = discord.Embed(title="Pong!", description=f"{round(self.client.latency * 1000)} MS", color=0x2f3136)
+        embed = discord.Embed(title="Pong!", description=f"{round(self.client.latency * 1000)}ms", color=embed_color)
         embed.timestamp = datetime.datetime.utcnow()
         await ctx.send(embed=embed) # fixed xd
 
 def setup(client):
-
     client.add_cog(Epic(client))
