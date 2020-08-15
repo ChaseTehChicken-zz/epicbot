e0b2c32a99df7ca556acbda6dc326d48a5b28b00
Chaseyy <38933237+ChaseTehChicken@users.noreply.github.com>
2020-08-15 22:42:59 +1000

    Testing push event

--- bot.py
+++ bot.py
@@ -19,4 +19,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
+        # Testing push event
+        
 client.run(token) # make token variable
